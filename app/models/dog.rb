75697ba78c479b70038b631e66606daf73a1bf4e
eanorton <edwardnorton@icloud.com>
2018-05-24 15:40:48 -0400

    done?

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,7 @@
 class Dog < ApplicationRecord
+  has_many :employees
+
+  validates :name, presence: true, uniqueness: true
+  validates :breed, presence: true
+  validates :age, presence: true
 end
